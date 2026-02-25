f54780857d5e8475bc81540d4f1287b997d5eac5
sunny kasana <sunnykasana252@gmail.com>
2026-02-25 14:31:23 +0530

    Initial commit

---
+++ javsscriptoperator.js
@@ -0,0 +1,9 @@
+let a = 10;
+let b = 5;
+
+console.log("Addition:", a + b);        
+console.log("Subtraction:", a - b);     
+console.log("Multiplication:", a * b);  
+console.log("Division:", a / b);        
+console.log("Modulus:", a % b);        
+      
