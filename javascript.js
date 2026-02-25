1f42680579ff4f61a4a577815357b7afcfea0274
sunny kasana <sunnykasana252@gmail.com>
2026-02-25 14:20:31 +0530

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,32 @@
+// console.log("hello world");
+// console.log("hii");
+// console.log(334)
+// console.log(3+4)
+//document.write("hello world");
+
+// var num = 10;
+// console.log(num);
+// console.log(typeof(num));
+// var num =false ;
+// console.log(num);
+// console.log(typeof(num))
+// var num =null ;
+// console.log(num);
+// console.log(typeof(num))
+// var num ="sunny" ;
+// console.log(num);
+// console.log(typeof(num))
+
+<script type="text/javascript">
+const sym1=symbol(4)
+console.log(sym1);
+const sym2=symbol(4);
+console.log(sym2);
+if(sym1==sym2)
+{
+    console.log("true");
+}
+else{
+    console.log("false");
+}
+</script>
