3bf59a90af0a7f14efb0adfb709efcdf1f955578
sunny kasana <sunnykasana252@gmail.com>
2026-03-18 14:52:03 +0530

    dom

---
+++ newscript.js
@@ -0,0 +1,51 @@
+// const x= document.getElementById("p1");
+// console.log(x);
+// x.innerHTML="my self";
+//  x.title="<b>Sunny kasana </b>";
+// const resut=document.getElementsByClassName("p1");
+// console.log(result);
+
+// result[0]=innerHTML="kasana";
+// result[1]=innerHTML="sunny";
+// const resut3=document.getElementsByTagName("p");
+// console.log(result);
+
+// result3[2].innerHTML="kasana";
+// result3[1].style.color="blue";
+// result3[1].innerTEXT="sunny";
+
+// const text2= document.querySelectorAll("ul li");
+// for(x in text2){
+//     text2.stle.backgroundcolor="green";
+//     text2.
+// }
+
+// const x=document.getElementByID("jk").getAttribute("class");
+// document.getElementById("d").innerHTml=x; 
+
+function onclick() {
+    alert("Button clicked!");
+}
+
+function onmouseout() {
+    alert(" on Mouse out ");
+}
+
+function onmousedown() {
+    alert("on Mouse down");
+}
+
+function onmouseup() {
+    alert("on Mouse up");
+}
+
+function onmouseremove() {
+    alert("on Mouse remove ");
+}
+
+function ondblclick() {
+    alert("on Button doubleclicked!");
+}
+function myfunction(){
+    alert("window load successful");
+}
